758e3941e4cab7fa31992596b3c65923c19e2137
Maximiliano Ahumada <maximiliano.ahumada@usach.cl>
2022-11-17 17:49:44 -0300

    modificacion servicio para generar marcas

--- src/main/java/Services/marcas_relojService.java
+++ src/main/java/Services/marcas_relojService.java
@@ -6,10 +6,8 @@ import Repository.marcas_relojRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.HttpEntity;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -31,6 +29,32 @@ public class marcas_relojService {
     public Marca_reloj save(Marca_reloj marca_reloj) {
         Marca_reloj nuevaMarca_reloj = marcas_relojRepository.save(marca_reloj);
         return nuevaMarca_reloj;
+    }
+
+    public List<Marca_reloj> generarMarcas(ArrayList<String> arrayLecturas){
+        //en esta lista se almacenan las marcas de reloj creadas para cada trabajador
+        List<Marca_reloj> marcasGeneradas = new ArrayList<Marca_reloj>();
+        ArrayList<String> rutsRegistrados = new ArrayList<String>();
+        Integer cantidadLecturas = arrayLecturas.size();
+        //ciclo para encontrar todos los ruts registrados
+        for (int i=0 ; i<cantidadLecturas ; i++){
+            String lecturaActual = arrayLecturas.get(i);
+            String[] lecturaSeparada = lecturaActual.split(";");
+            if(!rutsRegistrados.contains(lecturaSeparada[0])){
+                rutsRegistrados.add(lecturaSeparada[0]);
+            }
+        }
+        //ciclo para generar las marcas de cada uno de los ruts
+        for (int i=0 ; i<cantidadLecturas ; i++ ){
+            String lecturaMarcaActual = arrayLecturas.get(i);
+            String[] marcaActual = lecturaMarcaActual.split(";");
+            
+
+        }
+
+
+
+        return marcasGeneradas;
     }
 
 }
