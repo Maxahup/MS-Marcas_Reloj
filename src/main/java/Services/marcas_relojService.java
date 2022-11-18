a1e6c9854536b44501f1aee86b372fc52da08465
Maximiliano Ahumada <maximiliano.ahumada@usach.cl>
2022-11-18 01:11:44 -0300

    modificacion conteo retraso

--- src/main/java/Services/marcas_relojService.java
+++ src/main/java/Services/marcas_relojService.java
@@ -40,15 +40,53 @@ public class marcas_relojService {
         for (int i=0 ; i<cantidadLecturas ; i++){
             String lecturaActual = arrayLecturas.get(i);
             String[] lecturaSeparada = lecturaActual.split(";");
-            if(!rutsRegistrados.contains(lecturaSeparada[0])){
-                rutsRegistrados.add(lecturaSeparada[0]);
+            if(!rutsRegistrados.contains(lecturaSeparada[2])){
+                rutsRegistrados.add(lecturaSeparada[2]);
             }
         }
         //ciclo para generar las marcas de cada uno de los ruts
-        for (int i=0 ; i<cantidadLecturas ; i++ ){
-            String lecturaMarcaActual = arrayLecturas.get(i);
-            String[] marcaActual = lecturaMarcaActual.split(";");
-            
+        for (int i=0 ; i< rutsRegistrados.size() ; i++ ){
+            int min_10=0;
+            int min_25=0;
+            int min_45=0;
+            int ausencia=0;
+            int horasExtras=0;
+            for (int j =0; j< cantidadLecturas; j++){
+                String marcaActual = arrayLecturas.get(j);
+                String[] marcaSeparada = marcaActual.split(";");
+                if (marcaSeparada[2].equals(rutsRegistrados.get(i))){
+                    //obtiene la hor en formato hh:mm
+                    String[] horaMarcada=marcaSeparada[1].split(":");
+                    int horaLeida = Integer.parseInt(horaMarcada[0]);
+                    int minutoLeido = Integer.parseInt(horaMarcada[1]);
+                    //verificar ingreso con retraso, se acota hasta las 12 para evitar problemas con
+                    //hora de salida
+                    if (horaLeida>=7 && horaLeida<12){
+                        //retraso de llegada de entre 10 y 25 minutos
+                        if (minutoLeido >10 && minutoLeido <= 25){
+                            min_10=min_10+1;
+                        }
+                        if (minutoLeido >25 && minutoLeido <= 45){
+                            min_25=min_25+1;
+                        }
+                        if (minutoLeido >45 && minutoLeido <=59 || (minutoLeido >=0 && minutoLeido<=15 && horaLeida>=9 && horaLeida<=10)){
+                            min_45=min_45+1;
+                        }
+                        if (minutoLeido>15 && horaLeida>=9 && horaLeida<12){
+                            ausencia=ausencia+1;
+                        }
+                    }
+                    /*Este segmento de codigo se encuentra deshabilitad debido a no ser un requerimiento
+                    el implementar la autorizacion de horas extras (sin autorizacion no se pagan las horas extras)
+
+                    if (horaLeida>18 && horaLeida<24){
+                        horasExtras=horasExtras+(horaLeida-18);
+                    }
+                     */
+                }
+
+            }
+
 
         }
 
